47629436cc9791e8a47a0fc93302d218b753db3f
sy-md <73502678+sy-md@users.noreply.github.com>
2023-01-28 20:48:31 -0700

    update for system
    neovim messing up

--- farmig_app/farmig_app.py
+++ farmig_app/farmig_app.py
@@ -24,7 +24,17 @@ class AuthState(main_farm_app): #the login box
     confrim_password: str
     
     def signup(self): #method for the signup 
-        pass 
+         """Sign up a user."""
+        with pc.session() as session:
+            if self.password != self.confirm_password:
+                return pc.window_alert("Passwords do not match.")
+            if session.exec(User.select.where(User.username == self.username)).first():
+                return pc.window_alert("Username already exists.")
+            user = User(username=self.username, password=self.password)
+            session.add(user)
+            session.commit()
+            self.logged_in = True
+            return pc.redirect("/home")
     def login(self): #method for the login
         with pc.seesion() as session:
             user = session.exec(
@@ -37,9 +47,57 @@ class AuthState(main_farm_app): #the login box
                 return pc.window_alert("invalid username or password")
 
 def signup(): #when button is pressed 
-    return pc.text("signup page")
+    return pc.box(
+        pc.vstack(
+            navbar(State),
+            pc.center(
+                pc.vstack(
+                    pc.heading("Sign Up", font_size="1.5em"),
+                    pc.input(
+                        on_blur=State.set_username, placeholder="Username", width="100%"
+                    ),
+                    pc.input(
+                        on_blur=AuthState.set_password,
+                        placeholder="Password",
+                        width="100%",
+                    ),
+                    pc.input(
+                        on_blur=AuthState.set_confirm_password,
+                        placeholder="Confirm Password",
+                        width="100%",
+                    ),
+                    pc.button("Sign Up", on_click=AuthState.signup, width="100%"),
+                ),
+                style=styles["login_input"],
+            ),
+        ),
+        style=styles["login_page"],
+    )
 def login():
-    return pc.text("login page")
+        return pc.box(
+        pc.vstack(
+            navbar(State),
+            pc.center(
+                pc.vstack(
+                    pc.input(
+                        on_blur=State.set_username, placeholder="Username", width="100%"
+                    ),
+                    pc.input(
+                        on_blur=AuthState.set_password,
+                        placeholder="Password",
+                        type_="password",
+                        width="100%",
+                    ),
+                    pc.button("Login", on_click=AuthState.login, width="100%"),
+                    pc.link(
+                        pc.button("Sign Up", width="100%"), href="/signup", width="100%"
+                    ),
+                ),
+                style=styles["login_input"],
+            ),
+        ),
+        style=styles["login_page"],
+    )
 def home(): #when button is pressed
     return pc.vstack(
             pc.heading("farming App", size="4x1"),

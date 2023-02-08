c0dec5747fa309aa6053291ef7e38d584ccd3715
sy-md <mdorsetti733@gmail.com>
2023-02-08 15:48:03 -0700

    wq

--- engine.py
+++ engine.py
@@ -1,61 +1,21 @@
-import json 
+"""
+the middle-man engine
 
-file = "database.json"
+allows me to break doww dict better for the pynecone to handel them better
 
-tmp = []
+plus can be used a as json handler and holder user data
 
-database = {
-        "users" : {
-            "username" : "",
-            "password" : ""},
-        "farm"  : []
-        }
+"""
 
-def save_user(username="admin", password="0000"):
-    try:
-        with open(file, "r") as db:
-            data = json.load(db)
-
-    except FileNotFoundError:
-        with open(file, "w") as db:
-            json.dump(data, db, indent=4)
-
-    else: 
-        database["user"]["username"] = "admin"
-        database["user"]["password"] = "0000"
-
-        with open(file, "w") as db:
-            json.dump(database, db, indent=4)
-
-def update(tmp):
-    with open(file, "r") as db:
-        data = json.load(db)
-    with open(file, "w") as save_db:
-        json.dump(data, save_db, indent=4)
+def get_values(plant) -> list:
+    tmp = []
+    for v in plant.values():
+        tmp.append(v)
+    return tmp
 
 
-def gen(rows):
-    try:
-        with open(file, "r") as db:
-            data = json.load(db)
-
-            for x in rows.values():
-                data["farm"].append(x)
-
-            with open(file, "w") as save:
-                json.dump(data, save, indent=4)
-    except FileNotFoundError:
-        with open(file, "w") as db:
-            json.dump(database, db, indent=4)
-
-    with open(file, "r") as db:
-        data = json.load(db)
-
-        return data["farm"]
-
-
-
-
-def check_plnt(plant):
-    pass
-    
+def get_keys(plant) -> list:
+    tmp = []
+    for k in plant.keys():
+        tmp.append(k)
+    return tmp
